68a5c28c6bc13ffba67ffeb62579d0a53f0c5eb2
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-10-26 17:57:26 -0300

    melhorei comentários

--- conexao/emissor.js
+++ conexao/emissor.js
@@ -1,15 +1,19 @@
+// importando bibliotecas, classes e configurações
 const { Worker } = require('worker_threads')                // biblioteca para criar uma thread paralela que serve como o receptor
-const Channel = require('./canal')                     // canal
+const Channel = require('./canal')                          // canal
 const config = require('./canalConfig')                     // configuração do canal
-const print = require('../util/logger')('EMISSOR', '36m')    // print bonitinho
+const print = require('../util/logger')('EMISSOR', '36m')   // print bonitinho
 
-const tipoDeEmissor = 'rdt'                                     // qual protocolo o emissor está usando
-const senderLogicPath = `../algoritmos/${tipoDeEmissor}/sender`  // qual o caminho para o arquivo que implementa a lógica que o emissor está usando
+// escolhendo o tipo de conexão
+const protocolo = 'rdt'                                     // qual protocolo o emissor está usando
+const senderLogicPath = `../algoritmos/${protocolo}/sender` // qual o caminho para o arquivo que implementa a lógica que o emissor está usando
 
-const receptor = new Worker('./conexao/receptor.js')                // criar o receptor
+// esbelendo canal de comunicação com o receptor
+const receptor = new Worker('./conexao/receptor.js', { worketData: protocolo }) // criar o receptor
 const canal = new Channel(config)                           // criar o canal que simula atraso, perda de pacotes e limite de banda
 canal.setSender(msg => receptor.postMessage(msg))           // configurando para onde o canal deve mandar mensagens saindo do emissor (para o receptor)
 
+// instanciando a lógica de conexão
 const senderLogic = require(senderLogicPath)                // importando a lógica de conexão 
 const emissor = new senderLogic(canal)                      // criando uma nova instância da lógica de conexão
 receptor.on('message', message => emissor.recieve(message)) // redirecionando todas as mensagens recebidas para a lógica de conexão
@@ -19,5 +23,5 @@ receptor.on('message', msg => print(`eu recebi a mensagem`, msg))
 // TEMPORARIO ###############################################################
 
 module.exports = {
-  send: canal.send
+  send: pkt => print("enviando o pacote:", pkt) || canal.send(pkt)
 }
--- conexao/receptor.js
+++ conexao/receptor.js
@@ -1,23 +1,23 @@
 // importando bibliotecas, classes e configurações
-const { parentPort } = require('worker_threads')            // por onde o canal pode mandar mensagens para o emissor
-const config = require('./canalConfig')                     // configuração do canal
-const Channel = require('./canal')                     // canal de comunicação com o emissor
-const print = require('../util/logger')('RECEPTOR', '95m')   // print bonitinho
-const Packet = require('../util/packet')                     // criador de pacotes
+const { parentPort, workerData } = require('worker_threads')  // por onde o canal pode mandar mensagens para o emissor
+const config = require('./canalConfig')                       // configuração do canal
+const Channel = require('./canal')                            // canal de comunicação com o emissor
+const print = require('../util/logger')('RECEPTOR', '95m')    // print bonitinho
+const Packet = require('../util/packet')                      // criador de pacotes
 
 // estabelecendo canal
-const emissor = parentPort                                  // pegar referencia do receptor
-const canal = new Channel(config)                           // criando o canal de comunicação com o receptor
-canal.setSender(msg => emissor.postMessage(msg))            // configurando para onde o canal deve mandar mensagens saindo do receptor (para o emissor)
+const emissor = parentPort                                    // pegar referencia do receptor
+const canal = new Channel(config)                             // criando o canal de comunicação com o receptor
+canal.setSender(msg => emissor.postMessage(msg))              // configurando para onde o canal deve mandar mensagens saindo do receptor (para o emissor)
 
 // pegando o arquivo onde a lógica da conexão está implementada
-const tipoDeReceptor = 'rdt'                                        // qual protocolo o receptor está usando
-const recieverLogicPath = `../algoritmos/${tipoDeReceptor}/reciever` // qual o caminho para o arquivo que implementa a lógica que o receptor está usando
+const protocolo = workerData || 'rdt'                           // qual protocolo o receptor está usando
+const recieverLogicPath = `../algoritmos/${protocolo}/reciever` // qual o caminho para o arquivo que implementa a lógica que o receptor está usando
 
 // criando a lógica de conexão
-const recieverLogic = require(recieverLogicPath)            // importando a lógica de conexão 
-const receptor = new recieverLogic(canal)                   // criando uma nova instância da lógica de conexão
-emissor.on('message', message => receptor.recieve(message)) // redirecionando todas as mensagens recebidas para a lógica de conexão
+const recieverLogic = require(recieverLogicPath)              // importando a lógica de conexão 
+const receptor = new recieverLogic(canal)                     // criando uma nova instância da lógica de conexão
+emissor.on('message', message => receptor.recieve(message))   // redirecionando todas as mensagens recebidas para a lógica de conexão
 
 
 // isso é inútil, pode tirar quando o trabalho estiver pronto //////////////////////////////////////////////
