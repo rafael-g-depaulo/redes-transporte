c06a0ab7180f7c33be5c98725d1fc55f61d21a54
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-10-26 15:13:26 -0300

    implementei banda

--- canalConfig.js
+++ canalConfig.js
@@ -1,8 +1,8 @@
-module.export = {
+module.exports = {
   // quantos ms demora para o primeiro byte de um pacote que começou a ser enviado a chegar ao destino
-  atraso: 1000,
+  atraso: 1,
   // quantos bytes podem passar pelo canal por segundo
-  bandwidth: 500,
+  bandwidth: 1,
   // qual a porcentagem (0 a 1) de pacotes que se perdem no caminho
   lossRate: 0.5,
   // se o canal não tem perda de pacotes (se "true", é equivalente a lossRate = 0)
--- util/canal.js
+++ util/canal.js
@@ -1,9 +1,14 @@
 const delay = require('./delay')                  // adiciona delay
-const print = require('./logger')('CANAL', '92m') // print bonitinho
+const _print = require('./logger')('CANAL', '92m') // print bonitinho
+
+// se é pra mostrar na tela o que está acontecendo no canal
+const log = true
+const print = log ? _print : () => {}
 
 module.exports = class Channel {
   
   sender = null
+  aEnviar = []
 
   constructor({ atraso, bandwidth, lossRate, noLoss }) {
     this.atraso = atraso
@@ -11,24 +16,50 @@ module.exports = class Channel {
     this.lossRate = lossRate
     this.noLoss = noLoss
   }
+
+  // enquanto tiver um pacote a ser enviado, transmite eles para serem enviados
+  // OBS: manda 1 pacote por vez, e demora pacote.size / bandwith segundos para transmitir o pacote
+  sendLoop = () => {
+    // se não tem pacote a ser enviado, retorna
+    if (this.aEnviar.length === 0) return
+
+    // pegar o pacote a ser enviado
+    const { data, size } = this.aEnviar.shift()
+
+    // transima o pacote
+    delay(1000 * size / this.bandwidth)
+      .then(() => print("terminei de transmitir o pacote", { data, size }, ", agora a rede vai levar ele até o meu remetente"))
+      .then(() => print("a lista de pacotes que faltam ser enviados é", this.aEnviar))
+      .then(() => this.sendPacket({ data, size }))
+      .then(() => this.aEnviar.length !== 0 && this.sendLoop())  // mande o próx pacote, se existir
+  }
   
+  // manda um pacote pela conexão (que demora this.atraso ms para chegar)
+  sendPacket = packet => {
+    print("a rede está transportando o pacote:", packet, `mas a rede tem um delay de ${this.atraso}ms (${this.atraso/1000}s)`)
+    delay(this.atraso)
+      .then(() => Math.random() < this.lossRate && !this.noLoss               // A mensagem se perdeu no canal?
+        ? print(`oops, a rede perdeu o pacote`, packet, ` no caminho`)                    // sim, se perdeu no canal
+        : print(`o pacote foi entregue!`, packet) || this.sender(packet)      // não, foi entregue
+      )
+  }
+
   // por onde mandar mensagens
   setSender = sender => this.sender = sender
 
-  // mandar para a camada de rede o que a camada de transporte mandar
-  send = ({ data, size }) => {
+  // transmitir para a camada de rede o que a camada de transporte mandar
+  send = (...pacotes) => {
     
-    print("emissor/receptor me mandou mandar uma mensagem:", { data, size })
-    
-    // se a mensagem se perdeu no canal, e não é pra ser mandada
-    if (Math.random() < this.lossRate && !this.noLoss)
-      return print(`mas eu "perdi" a mensagem no meio do caminho kkkk`)
+    // aqui da pra botar condição de corromper os pacotes, se necessário
 
-    // aqui da pra botar condição de corromper a mensagem tbm, da mesma forma
-    
-    print(`mas eu vou dar um delay de ${this.atraso}ms (${this.atraso/1000}s)`)
-    delay(this.atraso)
-      .then(() => this.sender({ data, size, merda: "o canal adicionou lixo" }))
+    // se não está enviando pacotes (lista de envio vazia)
+    const comeceLoop = this.aEnviar.length === 0
+
+    // adicione os pacoter para serem enviados
+    this.aEnviar.push(...pacotes)
+
+    // se não está enviando pacotes, envie pacotes
+    if (comeceLoop) this.sendLoop()
   }
 
 }
