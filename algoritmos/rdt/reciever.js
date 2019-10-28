445db46ae99914361d60329f8927c27672d0742a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-10-28 10:03:27 -0300

    mudei nome de variável

--- algoritmos/rdt/reciever.js
+++ algoritmos/rdt/reciever.js
@@ -16,23 +16,21 @@ const print = config.print.receptorME ?                         // print bonitin
   
   // recebe mensagens por esse método
   recieve = packet => {
-    let aick
+    let ack
     if (this.isCorrupted(packet)) {//se estiver corrompido
       if (packet.header.ack == 0) {
-        aick = 1
-        this.send(new Packet(packet.data, { ack: aick }))
-      }
-      else{
-        aick = 0
-        this.send(new Packet(packet.data, { ack: aick }))
+        ack = 1
+        this.send(new Packet(packet.data, { ack }))
+      } else {
+        ack = 0
+        this.send(new Packet(packet.data, { ack }))
       }
     } else {//se nao estiver corrompido mas o ack estiver errado
         if (this.expect!= packet.header.ack){
           //me deixou confuso isso aqui. Como meu ACK veio errado (exemplo: Expected = 0 ACK1) eu vou enviar o ACK que EU recebi para o sender, que espera um ACK=0 mas recebe ACK =1
           print("Olha nem corrompeu mas ack ta baubau")
           this.send(packet)
-        }
-        else{//Se o ACK for o esperado
+        } else {//Se o ACK for o esperado
           //tenho umas dúvidas. Aqui eu envio esse pacote, porque como ele veio com o ack correto por exemplo, esperava 0 e ACK= 0, ele vai enviar o pacote com o ACK correto para o sender
           print("recebi o pacote deboinhas", packet)
           this.send(packet)
